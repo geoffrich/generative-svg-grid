8e4bd2e43b8af054608e2bce0539d176f6e6aa4a
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-02-11 17:35:06 -0800

    Mask letter block

--- src/lib/shapes/LetterBlock.svelte
+++ src/lib/shapes/LetterBlock.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { random } from '$lib/deps';
+	import { Mask } from '$lib/shapes';
 
 	export let size;
 	export let foreground;
@@ -42,16 +43,18 @@
 	let rotation = random([0, 90, 180, 270]);
 </script>
 
-<g class="letter-block">
-	<rect height={size} width={size} fill={background} {x} {y} />
-	<text
-		x={x + size / 2}
-		y={y + size / 2 + fontSize / 3}
-		transform="rotate({rotation}, {x + size / 2}, {y + size / 2})"
-		font-family="Source Code Pro"
-		font-size={fontSize}
-		font-weight="800"
-		text-anchor="middle"
-		fill={foreground}>{char}</text
-	>
-</g>
+<Mask {size} {x} {y} let:mask>
+	<g {mask} class="letter-block">
+		<rect height={size} width={size} fill={background} {x} {y} />
+		<text
+			x={x + size / 2}
+			y={y + size / 2 + fontSize / 3}
+			transform="rotate({rotation}, {x + size / 2}, {y + size / 2})"
+			font-family="Source Code Pro"
+			font-size={fontSize}
+			font-weight="800"
+			text-anchor="middle"
+			fill={foreground}>{char}</text
+		>
+	</g>
+</Mask>
